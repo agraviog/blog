b164acaa186cbe66479145abd8ec402a51db8ae4
agraviog <agraviog@gmail.com>
2023-03-27 18:19:17 +0800

    blog post

---
+++ pages/blogdata.js
@@ -0,0 +1,23 @@
+export const blogData = [
+  {
+    id: 'blog-001',
+    title: 'How I Taught Myself Coding and SEO and why you should too',
+    description:
+      'Learn why acquiring these skills can transform your career and business. Empower yourself with valuable insights and practical tips for mastering the digital world.',
+    date: 'March 27, 2023',
+    image:
+      'https://images.unsplash.com/photo-1605106702842-01a887a31122?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=774&q=80',
+    alt: 'Blog Image 01',
+  },
+  {
+    id: 'blog-002',
+    title:
+      'Live Third, Earn First: First-World Salary in a Third-World Setting',
+    description:
+      'Discover the secrets to earning a first-world salary while enjoying the low cost of living in a third-world setting.',
+    date: 'March 28, 2023',
+    image:
+      'https://images.unsplash.com/photo-1604782206219-3b9576575203?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2797&q=80',
+    alt: 'Blog Image 02',
+  },
+];
