5068a19fa1a3a4fdff05828b42eec646cd580c81
agraviog <agraviog@gmail.com>
2023-03-27 21:00:36 +0800

    commit

--- pages/blog/how-i-thought-myself-coding-and-seo.js
+++ pages/blog/how-i-thought-myself-coding-and-seo.js
@@ -1,5 +1,5 @@
 import Head from 'next/head';
-import styles from '/styles/blogPost.module.css';
+import styles from '/styles/Blog.module.css';
 import Nav from '../nav';
 import Footer from '../footer';
 import blogData from '/data/blogdata';
@@ -36,56 +36,12 @@ export default function blogPost() {
         <div className="innerWrapper">
           <div className={styles.richText}>
             <p>
-              If you're someone who's interested in learning new skills or
-              switching careers, then you've probably thought about learning how
-              to code or how to optimize websites for search engines. And if
-              you're like most people, you might assume that you need to go to
-              school or take a course to learn these skills. But that's not
-              true! With the right mindset and approach, anyone can teach
-              themselves coding and SEO.
-            </p>
-            <p>
-              As someone who taught themselves both of these skills, I'm here to
-              tell you that it's possible. And not only is it possible, but it
-              can also be incredibly rewarding. Here are a few reasons why you
-              should consider teaching yourself coding and SEO:
-            </p>
-            <ol>
-              <li>
-                It's a valuable skillset. In today's job market, coding and SEO
-                are in high demand. Many companies are looking for individuals
-                who can help them improve their websites and online presence. By
-                learning these skills, you'll be positioning yourself as a
-                valuable asset to potential employers or clients.
-              </li>
-              <li>
-                It's affordable. One of the biggest advantages of teaching
-                yourself coding and SEO is that it's much more affordable than
-                going to school or taking a course. All you need is a computer
-                and an internet connection, and you can access countless
-                resources to help you learn.
-              </li>
-              <li>
-                You can work from anywhere. Once you've developed your coding
-                and SEO skills, you'll have the flexibility to work from
-                anywhere in the world. Whether you want to work from home,
-                travel, or live abroad, you'll be able to take your work with
-                you.
-              </li>
-            </ol>
-            <p>
-              So, how did I teach myself coding and SEO? It wasn't easy, but it
-              was definitely worth it. I started by reading books and online
-              tutorials, and I practiced every day. I made a lot of mistakes
-              along the way, but I learned from them and kept going. Over time,
-              I developed my skills and built up my portfolio.
-            </p>
-            <p>
-              If you're interested in teaching yourself coding and SEO, my
-              advice to you is to just start. Don't worry about being perfect or
-              knowing everything right away. Just start learning, practice every
-              day, and be patient. It won't happen overnight, but with time and
-              dedication, you can teach yourself anything you set your mind to.
+              Learning coding and SEO can be valuable, affordable, and offer the
+              flexibility to work from anywhere. With the right mindset, anyone
+              can teach themselves these skills through online resources,
+              practice, and patience. Self-taught individuals can become
+              valuable assets to employers or clients, save money compared to
+              formal education, and enjoy the freedom to work from any location.
             </p>
           </div>
         </div>
--- pages/blog/live-third-earn-first.js
+++ pages/blog/live-third-earn-first.js
@@ -1,5 +1,5 @@
 import Head from 'next/head';
-import styles from '/styles/blogPost.module.css';
+import styles from '/styles/Blog.module.css';
 import Nav from '../nav';
 import Footer from '../footer';
 import blogData from '/data/blogdata';
@@ -36,61 +36,16 @@ export default function blogPost() {
         <div className="innerWrapper">
           <div className={styles.richText}>
             <p>
-              The rise of remote work has created opportunities for individuals
-              to work from anywhere in the world, and this has led to a shift in
-              how we think about the relationship between where we live and
-              where we work. For people in developing countries, this shift has
-              opened up new possibilities for improving their quality of life.
-              In this blog post, we'll explore the concept of "live third, earn
-              first" and how it can benefit people in developing countries.
-            </p>
-            <p>
-              The idea of "live third, earn first" is simple: it means living in
-              a developing country where the cost of living is low, while
-              earning a salary from a developed country where the pay is much
-              higher. This is possible because remote work has made it possible
-              for people to work for companies from anywhere in the world. As a
-              result, people in developing countries can earn a first-world
-              salary while living in a third-world country.
-            </p>
-            <p>
-              The benefits of this approach are clear. First, it allows people
-              to take advantage of the lower cost of living in developing
-              countries. This means that they can live a comfortable life while
-              earning a salary that would be considered very high in their home
-              country. This can be especially beneficial for people who are just
-              starting out in their careers, as they can get a foothold in their
-              chosen field while living comfortably.
-            </p>
-            <p>
-              Second, "live third, earn first" can help to promote economic
-              development in developing countries. When people earn a higher
-              salary, they have more money to spend in their local economy. This
-              can help to create new businesses and jobs, which can further
-              boost economic growth.
-            </p>
-            <p>
-              Finally, "live third, earn first" can help to bridge the gap
-              between developed and developing countries. By allowing people to
-              work together across borders, it promotes greater understanding
-              and collaboration between different cultures and nations.
-            </p>
-            <p>
-              Of course, there are some challenges to working remotely from a
-              developing country. Internet connectivity and infrastructure can
-              be less reliable, and there may be cultural differences that can
-              make it more difficult to communicate effectively with colleagues
-              from different countries. However, with the right support and
-              infrastructure, these challenges can be overcome.
-            </p>
-            <p>
-              In conclusion, "live third, earn first" is an innovative approach
-              to work that can benefit people in developing countries. It offers
-              the possibility of a high salary, a comfortable lifestyle, and the
-              opportunity to promote economic growth in their home country. As
-              remote work continues to grow in popularity, we can expect to see
-              more people around the world taking advantage of this approach to
-              work and life.
+              "Live third, earn first" is a concept where individuals live in
+              developing countries with low living costs while earning a higher
+              salary from developed countries through remote work. This approach
+              offers benefits such as a comfortable lifestyle, promotion of
+              economic growth in developing countries, and fostering
+              collaboration between different cultures and nations. Despite
+              challenges like unreliable internet connectivity and cultural
+              differences, "live third, earn first" is an innovative way to
+              improve quality of life and support economic development in the
+              era of remote work.
             </p>
           </div>
         </div>
